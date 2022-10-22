77893b82f8b14009a425b98611ebe5b35e1aa319
Git_Biene <willemsilias@yahoo.com>
2022-10-22 18:45:52 +0200

    Added data application on JTPA data set

---
+++ Data_application.R
@@ -0,0 +1,66 @@
+# DataApplicationJTPA zou ik nog eens moeten nalezen want ik was al best moe
+# toen ik het aan het maken was.
+
+
+# Clear workspace
+rm(list = ls())
+
+# Load necessary packages, functions and data
+fulldata<-read.csv("clean_dataset_JTPA.csv")
+library(MASS)
+library(nloptr)
+library(numDeriv)
+library(xtable)
+source("Functions.r")
+
+# Select all males that have children (fathers)
+dataset = fulldata[(fulldata$children == 1 & fulldata$male == 1), -c(1,5,8)]
+
+Y = as.matrix(log(dataset$days))
+Delta = as.matrix(dataset$delta)
+dataset$intercept=rep(1,nrow(dataset))
+
+# Create data matrix X of unconfounded predictors. The columns of X are:
+# [Intercept, age, hsged, white, married]
+X = as.matrix(subset(dataset, select = c(ncol(dataset),1,2,3,5)))
+
+# Define some useful variables
+parl=ncol(X)+2
+totparl=2*parl
+parlgamma=parl-1
+
+# Create data matrix of confounded variable. Z is the indicator whether the
+# participant actually participated in the study. (0 for no participation, 1 
+# otherwise). 
+Z = as.matrix(dataset$jtpa)
+
+# Create data matrix of instrumental variable. W is the indicator whether the 
+# participant was in the control or treatment group (0 and 1, respectively).
+W = as.matrix(dataset$treatment)
+
+# Create data matrix
+XandW = as.matrix(cbind(X,W))
+n=nrow(dataset)
+data=as.matrix(cbind(Y,Delta,X,Z,W))
+namescoef =  c("beta_{T,0}","beta_{T,1}","beta_{T,2}","beta_{T,3}","beta_{T,5}",
+               "alpha_T","lambda_T","beta_{C,0}","beta_{C,1}","beta_{C,2}",
+               "beta_{C,3}","beta_{C,5}","alpha_C","lambda_C","sigma_T",
+               "sigma_C","rho","theta")
+
+
+# We follow the approach as in Chapter 5 of Crommen, Van Keilegom (2022). Hence,
+# we will be comparing the models (1) assuming independence (2) assuming
+# dependence but no confounding and (3) our model. It would not make sense to
+# compare our model with the model of Crommen, Van Keilegom (2022) directly.
+# However, we will compare the estimated survival curves.
+
+
+############################### IMPOARTANT NOTE: ###############################
+# If in the estimation of the independence model, it should be                 #
+# VargammaI = HgammaI[...] instead of VargammaI = Hgamma[...], this still needs#
+# to be changed! Here we use VargammaI = Hgamma[...].                          #
+################################################################################
+
+init.value.theta <- 1
+DataApplicationJPTA(data, init.value.theta) # Takes about 1 minute to run
+
--- Functions.r
+++ Functions.r
@@ -127,7 +127,7 @@ LikGamma2 = function(par,Y,M){
 # joint model with dependent censoring and transformation #
 ###########################################################
 
-# - Doesn't assume endogeneity
+# - Used in second step of 2-step estimation
 # - Does assume dependent censoring
 # - Uses Yeo-Johnson transformation
 
@@ -2886,4 +2886,458 @@ SimulationCI22_SaraIlias = function(n, nsim, iseed, init.value.theta) {
   print.xtable(xtab3,file=paste0("YJ_IndEstV22_",n,".txt"),add.to.row=addtorow,append=TRUE,table.placement="!")
   print(xtab3, add.to.row=addtorow, include.colnames=TRUE)
 }
+
+
+DataApplicationJPTA <- function(data, init.value.theta) {
+  
+  n = nrow(data)
+  Y = data[,1]
+  Delta = data[,2]
+  X = data[,(4:parl)]
+  Z = data[,parl+1]
+  W = data[,parl+2]
+  XandW = as.matrix(cbind(data[,3],X,W))
+  
+  # Estimate V
+  gammaest <- nloptr(x0=rep(0,parlgamma),eval_f=LikGamma2,Y=Z,M=XandW,lb=c(rep(-Inf,parlgamma)),ub=c(rep(Inf,parlgamma)),
+                     eval_g_ineq=NULL,opts = list(algorithm = "NLOPT_LN_BOBYQA","ftol_abs"=1.0e-30,"maxeval"=100000,"xtol_abs"=rep(1.0e-30)))$solution
+  V <- (1-Z)*((1+exp(XandW%*%gammaest))*log(1+exp(XandW%*%gammaest))-(XandW%*%gammaest)*exp(XandW%*%gammaest))-Z*((1+exp(-(XandW%*%gammaest)))*log(1+exp(-(XandW%*%gammaest)))+(XandW%*%gammaest)*exp(-(XandW%*%gammaest)))
+  
+  # Create matrix of X, Z and V.
+  M <- cbind(data[,3:(1+parl)],V)
+  
+  # Create matrix of X, Z and W.
+  MnoV = data[,3:(2+parl)]
+  
+  init = c(rep(0,totparl), 1, 1, init.value.theta)
+  
+  # Independent model for starting values sigmas and theta.
+  parhat1 = nloptr(x0=c(init),eval_f=LikI,Y=Y,Delta=Delta,M=M,lb=c(rep(-Inf,totparl),1e-05,1e-5, 0),ub=c(rep(Inf,totparl),Inf,Inf, 2),
+                   eval_g_ineq=NULL,opts = list(algorithm = "NLOPT_LN_BOBYQA","ftol_abs"=1.0e-30,"maxeval"=100000,"xtol_abs"=rep(1.0e-30)))$solution
+  
+  #
+  # Our model: Taking into account Z is likely a confounded variable and that T 
+  #            and C are dependent
+  #
+  
+  # Create vector of initial values for the estimation of the parameters using the
+  # parhat1. The final vector will be of the following form. 
+  # [1:7]  : beta
+  # [8:14] : eta
+  # [15]   : sigma1
+  # [16]   : sigma2
+  # [17]   : rho
+  # [18]   : theta
+  
+  initd <- c(parhat1, parhat1[length(parhat1)])
+  initd[length(initd) - 1] <- 0
+  
+  parhat = nloptr(x0=initd,eval_f=LikF,Y=Y,Delta=Delta,M=M,lb=c(rep(-Inf,totparl),1e-05,1e-5,-1,0),ub=c(rep(Inf,totparl),Inf,Inf,1,2),
+                  eval_g_ineq=NULL,opts = list(algorithm = "NLOPT_LN_BOBYQA","ftol_abs"=1.0e-30,"maxeval"=100000,"xtol_abs"=rep(1.0e-30)))$solution
+  
+  # Concatenate estimated coefficients vector for the models for transformed event
+  # and censoring time with the vector of coefficients for the model
+  # Z = \gamma * W + V. This results in the following vector.
+  # [1:7]   : beta
+  # [8:14]  : eta
+  # [15]    : sigma1
+  # [16]    : sigma2
+  # [17]    : rho
+  # [18]    : theta
+  # [19:24] : gamma
+  parhatG = c(parhat,as.vector(gammaest))
+  
+  Hgamma = hessian(LikFG2,parhatG,Y=Y,Delta=Delta,M=MnoV,method="Richardson",method.args=list(eps=1e-4, d=0.0001, zer.tol=sqrt(.Machine$double.eps/7e-7), r=6, v=2, show.details=FALSE)) 
+  
+  # Select part of variance matrix pertaining to beta, eta, var1, var2, rho and theta
+  # (i.e. H_delta).
+  H = Hgamma[1:length(initd),1:length(initd)]
+  HI = ginv(H)
+  
+  Vargamma = Hgamma[1:length(initd),(length(initd)+1):(length(initd)+parlgamma)]
+  
+  prodvec = XandW[,1]
+  
+  for (i in 1:parlgamma) {
+    for (j in 2:parlgamma) {
+      if (i<=j){
+        prodvec<-cbind(prodvec,diag(XandW[,i]%*%t(XandW[,j])))
+      }
+    }
+  }
+  
+  secder=t(-dlogis(XandW%*%gammaest))%*%prodvec
+  
+  WM = secder[1:parlgamma]
+  for (i in 2:parlgamma) {
+    newrow<-secder[c(i,(i+2):(i+parlgamma))]
+    WM<-rbind(WM,newrow) 
+  }
+  
+  WMI = ginv(WM)
+  
+  diffvec = Z-plogis(XandW%*%gammaest)
+  
+  mi = c()
+  
+  for(i in 1:n){
+    newrow<-diffvec[i,]%*%XandW[i,]
+    mi = rbind(mi,newrow)
+  }
+  
+  psii = -WMI%*%t(mi)
+  
+  gi = c()
+  
+  for (i in 1:n)
+  {
+    J1 = jacobian(LikF,parhat,Y=Y[i],Delta=Delta[i],M=t(M[i,]),method="Richardson",method.args=list(eps=1e-4, d=0.0001, zer.tol=sqrt(.Machine$double.eps/7e-7), r=6, v=2, show.details=FALSE))
+    gi = rbind(gi,c(J1))
+  }
+  
+  gi = t(gi)
+  
+  partvar = gi + Vargamma%*%psii
+  
+  Epartvar2 = (partvar%*%t(partvar))
+  
+  totvarex = HI%*%Epartvar2%*%t(HI)
+  
+  se = sqrt(abs(diag(totvarex)))
+  
+  # Delta method variance
+  
+  se_s1 = 1/parhat[totparl+1]*se[totparl+1]
+  se_s2 = 1/parhat[totparl+2]*se[totparl+2]
+  
+  # Conf. interval for transf. sigma's
+  
+  st1_l = log(parhat[totparl+1])-1.96*se_s1 ;  st1_u = log(parhat[totparl+1])+1.96*se_s1  
+  st2_l = log(parhat[totparl+2])-1.96*se_s2 ;  st2_u = log(parhat[totparl+2])+1.96*se_s2 
+  
+  # Back transform
+  
+  s1_l = exp(st1_l); s1_u = exp(st1_u); s2_l = exp(st2_l); s2_u = exp(st2_u) 
+  
+  # Confidence interval for rho
+  
+  zt = 0.5*(log((1+parhat[totparl+3])/(1-parhat[totparl+3])))     # Fisher's z transform
+  se_z = (1/(1-parhat[totparl+3]^2))*se[totparl+3]
+  zt_l = zt-1.96*(se_z)
+  zt_u = zt+1.96*(se_z)
+  
+  # Back transform
+  
+  r_l = (exp(2*zt_l)-1)/(exp(2*zt_l)+1)      
+  r_u = (exp(2*zt_u)-1)/(exp(2*zt_u)+1)
+  
+  # Confidence interval for theta
+  
+  rtheta_l <- parhat[length(parhat)] - 1.96 * se[length(parhat)]
+  rtheta_u <- parhat[length(parhat)] + 1.96 * se[length(parhat)]
+  
+  # Matrix with all confidence intervals
+  EC1 = cbind(matrix(c(parhat[1:totparl]-1.96*(se[1:totparl]),s1_l,s2_l,r_l,rtheta_l),ncol=1),
+              matrix(c(parhat[1:totparl]+1.96*(se[1:totparl]),s1_u,s2_u,r_u,rtheta_u), ncol=1))
+  
+  
+  #
+  # Naive model: assuming Z is an unconfounded variable, but including dependence
+  #              between T and C.
+  #
+  
+  # remove data for v from data matrix
+  ME = M[,-ncol(M)]
+  
+  # Remove coefficients for v in the vector parhat1. Add starting value for rho.
+  # The final vector will be of the form:
+  # [1:6]  : beta
+  # [7:12] : eta
+  # [13]   : sigma1
+  # [14]   : sigma2
+  # [15]   : rho
+  # [16]   : theta
+  
+  # Remove coefficients for v
+  initE = parhat1[-parl]
+  initE = initE[-(2*parl-1)]
+  
+  # Append theta to initE and replace the original theta (now second-to-last
+  # element) with the initial value for rho.
+  initE = c(initE,initE[length(initE)])
+  initE[length(initE) - 1] <- 0
+  
+  # Estimate the parameters
+  parhatE = nloptr(x0=initE,eval_f=LikF,Y=Y,Delta=Delta,M=ME,lb=c(rep(-Inf,(totparl-2)),1e-05,1e-5,-1,0),ub=c(rep(Inf,(totparl-2)),Inf,Inf,1,2),
+                   eval_g_ineq=NULL,opts = list(algorithm = "NLOPT_LN_BOBYQA","ftol_abs"=1.0e-30,"maxeval"=100000,"xtol_abs"=rep(1.0e-30)))$solution
+  
+  H1 = hessian(LikF,parhatE,Y=Y,Delta=Delta,M=ME,method="Richardson",method.args=list(eps=1e-4, d=0.0001, zer.tol=sqrt(.Machine$double.eps/7e-7), r=6, v=2, show.details=FALSE)) 
+  H1I = ginv(H1)
+  se1 = sqrt(abs(diag(H1I)));
+  
+  t_s1 = 1/parhatE[totparl-1]*se1[totparl-1]
+  t_s2 = 1/parhatE[totparl]*se1[totparl]
+  
+  # Conf. interval for transf. sigma's
+  
+  ms1_l = log(parhatE[totparl-1])-1.96*t_s1 ;  ms1_u = log(parhatE[totparl-1])+1.96*t_s1 
+  ms2_l = log(parhatE[totparl])-1.96*t_s2 ;  ms2_u = log(parhatE[totparl])+1.96*t_s2 
+  
+  # Back transform
+  
+  S1_l = exp(ms1_l); S1_u = exp(ms1_u); S2_l = exp(ms2_l); S2_u = exp(ms2_u) 
+  
+  # Confidence interval for rho
+  
+  z1t = 0.5*(log((1+parhatE[totparl+1])/(1-parhatE[totparl+1])))     # Fisher's z transform
+  se1_z = (1/(1-parhatE[totparl+1]^2))*se1[totparl+1]
+  z1t_l = z1t-1.96*(se1_z)
+  z1t_u = z1t+1.96*(se1_z)
+  
+  # Back transform
+  
+  r1_l = (exp(2*z1t_l)-1)/(exp(2*z1t_l)+1)      
+  r1_u = (exp(2*z1t_u)-1)/(exp(2*z1t_u)+1)
+  
+  # Confidence interval for theta
+  
+  r1theta_l <- parhatE[length(parhatE)] - 1.96 * se1[length(parhatE)]
+  r1theta_u <- parhatE[length(parhatE)] + 1.96 * se1[length(parhatE)]
+  
+  # Matrix of all the confidence intervals
+  EC2 = cbind(matrix(c(parhatE[1:(totparl-2)]-1.96*(se1)[1:(totparl-2)],S1_l,S2_l,r1_l, r1theta_l),ncol=1),
+              matrix(c(parhatE[1:(totparl-2)]+1.96*(se1)[1:(totparl-2)],S1_u,S2_u,r1_u, r1theta_u),ncol=1))
+  
+  
+  #
+  # Independence model: Model taking into account that Z is likely a confounded
+  #                     variable but assuming independence between T and C.
+  #
+  
+  # We construct the vector with
+  # [1:7]   : beta
+  # [8:14]  : eta
+  # [15]    : sigma1
+  # [16]    : sigma2
+  # [17]    : theta
+  # [18:23] : gamma
+  
+  parhatGI = c(parhat1,as.vector(gammaest))
+  
+  HgammaI = hessian(LikIGamma2,parhatGI,Y=Y,Delta=Delta,M=MnoV,method="Richardson",method.args=list(eps=1e-4, d=0.0001, zer.tol=sqrt(.Machine$double.eps/7e-7), r=6, v=2, show.details=FALSE)) 
+  
+  HInd = HgammaI[1:(length(initd)-1),1:(length(initd)-1)]
+  HIInd = ginv(HInd)
+  
+  VargammaI = Hgamma[1:(length(initd)-1),(length(initd)):(length(initd)+parlgamma-1)]
+  
+  giI = c()
+  
+  for (i in 1:n) {
+    J1I = jacobian(LikI,parhat1,Y=Y[i],Delta=Delta[i],M=t(M[i,]),method="Richardson",method.args=list(eps=1e-4, d=0.0001, zer.tol=sqrt(.Machine$double.eps/7e-7), r=6, v=2, show.details=FALSE))
+    giI = rbind(giI,c(J1I))
+  }
+  
+  giI = t(giI)
+  
+  partvarI = giI + VargammaI%*%psii
+  
+  Epartvar2I = (partvarI%*%t(partvarI))
+  
+  totvarexI = HIInd%*%Epartvar2I%*%t(HIInd)
+  
+  seI = sqrt(abs(diag(totvarexI)))
+  
+  # Delta method variance
+  
+  se_s1I = 1/parhat1[totparl+1]*seI[totparl+1]
+  se_s2I = 1/parhat1[totparl+2]*seI[totparl+2]
+  
+  # Conf. interval for transf. sigma's
+  
+  st1_lI = log(parhat1[totparl+1])-1.96*se_s1I ;  st1_uI = log(parhat1[totparl+1])+1.96*se_s1I  
+  st2_lI = log(parhat1[totparl+2])-1.96*se_s2I ;  st2_uI = log(parhat1[totparl+2])+1.96*se_s2I 
+  
+  # Back transform
+  
+  s1_lI = exp(st1_lI); s1_uI = exp(st1_uI); s2_lI = exp(st2_lI); s2_uI = exp(st2_uI)
+  
+  # Confidence interval for theta
+  
+  rItheta_l <- parhat1[length(parhat1)] - 1.96 * seI[length(parhat1)]
+  rItheta_u <- parhat1[length(parhat1)] + 1.96 * seI[length(parhat1)]
+  
+  EC4 = cbind(matrix(c(parhat1[1:totparl]-1.96*(seI[1:totparl]),s1_lI,s2_lI,rItheta_l),ncol=1),
+              matrix(c(parhat1[1:totparl]+1.96*(seI[1:totparl]),s1_uI,s2_uI,rItheta_u), ncol=1))
+  
+  
+  
+  # Results of model assuming confounding and dependence between T and C.
+  pvalue <- 2*pmin((1-pnorm(parhat/se)),pnorm(parhat/se))
+  significant <- ifelse(pvalue < 0.10,
+                        ifelse(pvalue < 0.05,
+                               ifelse(pvalue < 0.01, "**", "*"),"."), "")
+  results.confound_dep <- cbind(parhat, se, pvalue, EC1)
+  colnames(results.confound_dep) <- c("Estimate", "St.Dev.", "p", "CI.lb", "CI.ub")
+  rownames(results.confound_dep) <- namescoef
+  
+  summary <- data.frame(round(results.confound_dep, 3))
+  summary$sign <- significant
+  summary <- summary[,c(1:3, 6, 4:5)]
+  summary
+  
+  # Results of naive model
+  pvalue.naive <- 2*pmin((1-pnorm(parhatE/se1)),pnorm(parhatE/se1))
+  significant.naive <- ifelse(pvalue.naive < 0.10,
+                              ifelse(pvalue.naive < 0.05,
+                                     ifelse(pvalue.naive < 0.01, "**", "*"),"."), "")
+  results.naive <- cbind(parhatE, se1, pvalue.naive, EC2)
+  colnames(results.naive) <- c("Estimate", "St.Dev.", "pvalue", "CI.lb", "CI.ub")
+  rownames(results.naive) <- namescoef[-c(parl, totparl)]
+  
+  summary1 <- data.frame(round(results.naive, 3))
+  summary1$sign <- significant.naive
+  summary1 <- summary1[,c(1:3, 6, 4:5)]
+  summary1
+  
+  # Results of independence model
+  pvalue.indep <- 2*pmin((1-pnorm(parhat1/seI)),pnorm(parhat1/seI))
+  significant.indep <- ifelse(pvalue.indep < 0.10,
+                              ifelse(pvalue.indep < 0.05,
+                                     ifelse(pvalue.indeo < 0.01, "**", "*"),"."), "")
+  results.indep <- cbind(parhat1, seI, pvalue.indep, EC4)
+  colnames(results.indep) <- c("Estimate", "St.Dev.", "pvalue", "CI.lb", "CI.ub")
+  rownames(results.indep) <- namescoef[-(length(namescoef) - 1)]
+  
+  summary2 <- data.frame(round(results.indep, 3))
+  summary2$sign <- significant.indep
+  summary2 <- summary2[,c(1:3, 6, 4:5)]
+  summary2
+  
+  ## Create LaTeX tables of results
+  xtab = xtable(summary)
+  header= c("sample size",n,"Results 2-step_Estimation with YT-transformation")
+  addtorow = list()
+  addtorow$pos = list(-1)
+  addtorow$command = paste0(paste0('& \\multicolumn{1}{c}{', header, '}', collapse=''), '\\\\')
+  print(xtab, add.to.row=addtorow, include.colnames=TRUE)
+  
+  # print.xtable(xtab,file=paste0("Results_2-step_Estimation_YT",".txt"),add.to.row=addtorow,append=TRUE,table.placement="!")
+  
+  
+  xtab = xtable(summary1)
+  header= c("sample size",n,"Results naive model with YT-transformation")
+  addtorow = list()
+  addtorow$pos = list(-1)
+  addtorow$command = paste0(paste0('& \\multicolumn{1}{c}{', header, '}', collapse=''), '\\\\')
+  print(xtab, add.to.row=addtorow, include.colnames=TRUE)
+  
+  # print.xtable(xtab,file=paste0("Results_naive_YT",".txt"),add.to.row=addtorow,append=TRUE,table.placement="!")
+  
+  
+  xtab = xtable(summary2)
+  header= c("sample size",n,"Results independence model with YT-transformation")
+  addtorow = list()
+  addtorow$pos = list(-1)
+  addtorow$command = paste0(paste0('& \\multicolumn{1}{c}{', header, '}', collapse=''), '\\\\')
+  print(xtab, add.to.row=addtorow, include.colnames=TRUE)
+  
+  # print.xtable(xtab,file=paste0("Results_independence_YT",".txt"),add.to.row=addtorow,append=TRUE,table.placement="!")
+  
+  
+  #
+  # Plots of the estimated survival curves.
+  #
+  
+  ################## Model with Yeo-Johnson transformation #######################
+
+  S <- NULL
+  theta <- parhat[length(parhat)]
+  s1 <- parhat[totparl + 1]
+  
+  # parameter vector:
+  # c(intercept, age, has_highschool_degree(1=yes), white(yes=1), married(yes=1),
+  #   participated_in_study(no=0, otherwise=1),
+  #   assigned_group(control=0, treatment = 1))
+  dd <- c(1, 22, 1, 1, 0, 1, 1)
+  
+  Time <- sort(exp(Y))
+  
+  for (i in 1:length(Time)) {
+    sd = (YJtrans(log(Time[i]), theta) - t(parhat[1:7]) %*% dd)/s1
+    S[i] = 1 - pnorm(sd)
+  }
+  
+  ################ Model without Yeo-Johnson transformation ######################
+  
+  LikI_noTransform = function(par,Y,Delta,M) { # From Crommen, VK (2022)
+    M=as.matrix(M)
+    k = ncol(M)
+    l = 2*k
+    v = k+1
+    beta = as.matrix(par[1:k])
+    eta = as.matrix(par[v:l])
+    sigma1 = par[l+1]
+    sigma2 = par[l+2]
+    
+    z1 = (Y-(M%*%beta))/sigma1
+    z2 = (Y-(M%*%eta))/sigma2
+    
+    tot = (((1/sigma1)*dnorm(z1)*(1-pnorm(z2)))^Delta)*(((1/sigma2)*dnorm(z2)*(1-pnorm(z1)))^(1-Delta))
+    p1 = pmax(tot,1e-100)
+    Logn = sum(log(p1)); 
+    return(-Logn)
+  }
+  
+  LikF_noTransform = function(par,Y,Delta,M){ # From Crommen, VK (2022)
+    M=as.matrix(M)
+    k = ncol(M)
+    l = 2*k
+    v = k+1
+    beta = as.matrix(par[1:k])
+    eta = as.matrix(par[v:l])
+    sigma1 = par[l+1]
+    sigma2 = par[l+2]
+    rho = par[l+3]
+    
+    z1 = (Y-(M%*%beta))/sigma1
+    z2 = ((1-(rho*sigma2/sigma1))*Y-(M%*%eta-rho*(sigma2/sigma1)*(M%*%beta)))/(sigma2*((1-rho^2)^0.5))
+    z3 = (Y-(M%*%eta))/sigma2
+    z4 = ((1-(rho*sigma1/sigma2))*Y-(M%*%beta-rho*(sigma1/sigma2)*(M%*%eta)))/(sigma1*(1-rho^2)^0.5)
+    tot = (((1/sigma1)*dnorm(z1)*(1-pnorm(z2)))^Delta)*((1/sigma2)*dnorm(z3)*(1-pnorm(z4)))^(1-Delta)
+    p1 = pmax(tot,1e-100)   
+    Logn = sum(log(p1)); 
+    return(-Logn)
+  }
+  
+  # Create matrix of X, Z and V.
+  M = cbind(data[,3:(1+parl)],V)
+  
+  init = c(rep(0,totparl),1,1) # Starting values
+  
+  # Independent model for starting values:
+  parhat1 <- nloptr(x0=c(init),eval_f=LikI_noTransform,Y=Y,Delta=Delta,M=M,lb=c(rep(-Inf,totparl),1e-05,1e-5),ub=c(rep(Inf,totparl),Inf,Inf),
+                    eval_g_ineq=NULL,opts = list(algorithm = "NLOPT_LN_BOBYQA","ftol_abs"=1.0e-30,"maxeval"=100000,"xtol_abs"=rep(1.0e-30)))$solution
+  initd = c(parhat1,0)
+  parhat_noTransform <- nloptr(x0=initd,eval_f=LikF_noTransform,Y=Y,Delta=Delta,M=M,lb=c(rep(-Inf,totparl),1e-05,1e-5,-1),ub=c(rep(Inf,totparl),Inf,Inf,1),
+                               eval_g_ineq=NULL,opts = list(algorithm = "NLOPT_LN_BOBYQA","ftol_abs"=1.0e-30,"maxeval"=100000,"xtol_abs"=rep(1.0e-30)))$solution
+  
+  
+  S_noTransform <- NULL
+  s1_noTransform <- parhat[totparl + 1]
+  
+  for (i in 1:length(Time)) {
+    sd = (log(Time[i]) - t(parhat_noTransform[1:7]) %*% dd)/s1_noTransform
+    S_noTransform[i] = 1 - pnorm(sd)
+  }
+  
+  
+  plot(Time, S, type = 's', col = 1)
+  lines(Time, S_noTransform, type = 's', col = 2)
+  
+  # Add legend
+  legend(x = 1000, y = 0.95, c("Transformation model", "No transformation"),
+         col = c(1, 2), lty = 1)
+}
+
 
