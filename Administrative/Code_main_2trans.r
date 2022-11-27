3233f5a7cbda8eead31bed8d300b250f9725134b
Git_Biene <willemsilias@yahoo.com>
2022-11-27 16:37:30 +0100

    correction

--- Administrative/Code_main_2trans.r
+++ Administrative/Code_main_2trans.r
@@ -1,6 +1,3 @@
-# Clear workspace
-rm(list = ls())
-
 library(MASS)
 library(nloptr)
 library(numDeriv)
@@ -24,13 +21,14 @@ namescoef =  c("beta_{T,0}","beta_{T,1}","alpha_T","lambda_T","beta_{C,0}","beta
 # 1 = continuous and 2 = binary, the first number indicates Z and the second number indicates W
 # the order below also matches the design order in section 4 (simulation study) of the paper
 
-n <- 250
+n <- 10000
 nsim <- 300
 iseed <- 747852
 
 # To split up simulation in different parts
-number.of.parts <- 100
-parts.to.evaluate <- 1:2
+number.of.parts <- 30
+parts.to.evaluate <- 1:15
+
 for (part.to.evaluate in parts.to.evaluate) {
   message("Evaluating part ", which(part.to.evaluate == parts.to.evaluate),
           " out of ", length(parts.to.evaluate))
