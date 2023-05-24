28abffe7ccd1c04da8fa5709b18e4b0ba21f6431
Sara-Rutten <94563742+Sara-Rutten@users.noreply.github.com>
2023-05-24 13:44:35 +0200

    Update Data_application_cmprsk.R

--- Competing Risks/Data_application_cmprsk.R
+++ Competing Risks/Data_application_cmprsk.R
@@ -3,7 +3,7 @@
 rm(list = ls())
 
 # Load necessary packages, functions and data
-fulldata<-read.csv("hiip_data_041515.csv") # This data is not freely available
+fulldata<-read.csv("hiip_data_041515.csv") # The data collected by the HIP Breast Cancer Screening Trial is not freely available. It can be requested via the National Cancer Institute (NCI).
 library(MASS)
 library(nloptr)
 library(numDeriv)
